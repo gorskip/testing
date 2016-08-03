8358c73ddd308a2860067ace187de1be0e350c94
gorskip <prz.gorski@gmail.com>
2016-08-03 18:41:41 +0100

    changed type of params for TestParams

--- Boar/src/main/java/pl/ui/pg/boar/testdata/data/TestParams.java
+++ Boar/src/main/java/pl/ui/pg/boar/testdata/data/TestParams.java
@@ -2,18 +2,19 @@ package pl.ui.pg.boar.testdata.data;
 
 import java.util.List;
 import java.util.Map;
+import pl.ui.pg.boar.testdata.TestData;
 
 /**
  *
- * @author Przemo
+ * @author gorskip
  */
-public class TestParams {
+public class TestParams extends TestData {
 
     private final String id;
     private final String group;
     private final String description;
     private final List<String> userRoles;
-    private Map<String, String> params;
+    private Map<String, Object> params;
 
     public TestParams(String id, String group, String description, List<String> userRoles) {
         this.id = id;
@@ -38,15 +39,20 @@ public class TestParams {
         return userRoles;
     }
 
-    public Map<String, String> getParams() {
+    public Map<String, Object> getParams() {
         return params;
     }
 
-    public String getParam(String paramName) {
+    public Object getParam(String paramName) {
         return params.get(paramName);
     }
 
-    public void setParam(String paramName, String value) {
+    public void setParam(String paramName, Object value) {
         params.put(paramName, value);
     }
+
+    public void setParams(Map<String, Object> params) {
+        this.params = params;
+    }
+
 }
