b5c2a2ca0834d6c6f7a7ec5c0c92b52a4356086d
gorskip <prz.gorski@gmail.com>
2016-08-06 15:20:30 +0100

    added method to read file lines to list

--- Boar/src/main/java/pl/ui/pg/boar/utils/BoarFileUtils.java
+++ Boar/src/main/java/pl/ui/pg/boar/utils/BoarFileUtils.java
@@ -1,21 +1,26 @@
-package pl.ui.pg.boar.util;
+package pl.ui.pg.boar.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import org.apache.commons.io.FileUtils;
 
 /**
  *
  * @author gorskip
  */
-public class BoarFileUtil {
+public class BoarFileUtils {
 
     private static final String ENCODING = "UTF-8";
 
-    private BoarFileUtil() {
+    private BoarFileUtils() {
     }
 
     public static String readFile(File file) throws IOException {
         return FileUtils.readFileToString(file, ENCODING);
+    }
+
+    public static List<String> readLines(File file) throws IOException {
+        return FileUtils.readLines(file, ENCODING);
     }
 }
