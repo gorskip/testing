740b71ca5b453afebc65761c186dc1749414bf57
gorskip <prz.gorski@gmail.com>
2016-08-06 19:55:16 +0100

    added configuration methods amd testmg preparation

--- Boar/src/main/java/pl/ui/pg/boar/Boar.java
+++ Boar/src/main/java/pl/ui/pg/boar/Boar.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import org.testng.TestNG;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
 import pl.ui.pg.boar.config.DriverConfig;
 import pl.ui.pg.boar.config.DriverConfigBuilder;
+import pl.ui.pg.boar.config.loader.XmlClassLoader;
 import pl.ui.pg.boar.config.reader.DriverConfigReader;
 import pl.ui.pg.boar.exceptions.UserWithRoleNotProvidedException;
 import pl.ui.pg.boar.testdata.data.TestParams;
@@ -25,8 +30,10 @@ public class Boar {
     private List<TestParams> testParams;
     private List<User> users;
     private List<String> scenarios;
+    private String jarPath;
+    private List<XmlClass> xmlClasses;
 
-    public Boar prepareDriverConfiguration(Path driverConfigPath) {
+    public Boar setDriverConfiguration(Path driverConfigPath) {
         try {
             DriverConfigReader driverConfigReader = new DriverConfigReader(driverConfigPath);
             driverConfig = new DriverConfigBuilder(driverConfigReader.getImplicitWait())
@@ -39,19 +46,25 @@ public class Boar {
         return this;
     }
 
-    public Boar prepareTestParams(TestParamsProvider testParamsProvider) {
+    public Boar setTestParams(TestParamsProvider testParamsProvider) {
         testParams = testParamsProvider.build();
         return this;
 
     }
 
-    public Boar prepareUserParams(UserProvider userProvider) {
+    public Boar setUserParams(UserProvider userProvider) {
         users = userProvider.build();
         return this;
     }
 
-    public Boar prepareScenarios(ScenariosProvider scenariosProvider) {
+    public Boar setScenarios(ScenariosProvider scenariosProvider) {
         scenarios = scenariosProvider.build();
+        return this;
+    }
+
+    public Boar setTestJar(String testJarPath, String testPackage) {
+        jarPath = testJarPath;
+        xmlClasses = new XmlClassLoader().load(jarPath, testPackage);
         return this;
     }
 
@@ -85,6 +98,22 @@ public class Boar {
     }
 
     private void executeTest(TestParams testParamToTest, User user) {
+        TestNG testNG = prepareTestNG();
+        testNG.run();
+    }
+
+    private TestNG prepareTestNG() {
+        XmlSuite suite = new XmlSuite();
+        suite.setName("Boar");
+        XmlTest test = new XmlTest(suite);
+        test.setName("BoarTest");
+        test.setIncludedGroups(scenarios);
+        test.setXmlClasses(xmlClasses);
+        List<XmlSuite> suites = new ArrayList<>();
+        suites.add(suite);
+        TestNG testNG = new TestNG();
+        testNG.setXmlSuites(suites);
+        return testNG;
     }
 
 }
